f0c08e16a3e55f8a08121a75af135a2772ec3333
a.filipchik <4tefk3j5>
2022-04-28 14:44:08 +0300

    Revert "Scheduler_block"
    This reverts commit 7656b998f0a123bd33d08d3fb2bbb969c2d13195.

--- sidebars.js
+++ sidebars.js
@@ -559,7 +559,6 @@ module.exports = {
                 'Properties_and_actions_block', 
                 'Filters_and_sortings_block', 
                 'Event_block', 
-                'Scheduler_block',
               ]
             },
             'DESIGN_statement', 
